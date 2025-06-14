314c874f6a6ee72bfd8b55a6d6d6d9c1a042302b
Furkan <frknyrdm0@gmail.com>
2025-06-14 18:18:26 +0300

    v0.2

---
+++ components/LinksDropdown.tsx
@@ -0,0 +1,7 @@
+function LinksDropdown() {
+  return (
+    <h1 className="text-4xl">LinksDropdown</h1>
+  )
+  
+}
+export default LinksDropdown
---
+++ components/ThemeToggle.tsx
@@ -0,0 +1,4 @@
+function ThemeToggle() {
+  return <h1 className="text-4xl">ThemeToggle</h1>;
+}
+export default ThemeToggle;
