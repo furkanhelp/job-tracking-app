fe4a7c015c15e652503923860bd464b341987771
Furkan <frknyrdm0@gmail.com>
2025-06-15 18:44:28 +0300

    v0.3

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Providers from "./providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,14 +27,17 @@ export default function RootLayout({
   return (
 <ClerkProvider
       appearance={{
-        variables: { colorPrimary: '#000000' } // Optional: customize appearance
+        variables: { colorPrimary: '#000000' }
       }}
-      afterSignOutUrl="/" // Add this line
+      afterSignOutUrl="/"
       >
 
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+       <Providers>
+
         {children}
+       </Providers>
       </body>
     </html>
       </ClerkProvider>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,17 @@
 import LinksDropdown from "./LinksDropdown";
 import { UserButton } from "@clerk/nextjs";
-import ThemeToggle from "./ThemeToggle";
+import ThemeToggle from "../components/ThemeToggle";
 
 function Navbar() {
   return (
     <nav className="bg-muted py-4 sm:px-16 lg:px-24 px-4 
     flex items-center justify-between">
+      <div>
       <LinksDropdown/>
+
+      </div>
       <div className="flex items-center gap-x-4">
-        <ThemeToggle/>
+      <ThemeToggle/>
         <UserButton afterSignOutUrl="/"/>
       </div>
     </nav>
