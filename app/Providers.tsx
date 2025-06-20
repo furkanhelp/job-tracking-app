0115d7cd90734124bd83ad30e0c20f1935a3fcb2
Furkan <frknyrdm0@gmail.com>
2025-06-20 18:44:33 +0300

    v0.9.5

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -5,33 +5,32 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "@/components/ui/sonner";
 
-function providers({ children}: {children: React.ReactNode}) {
-  const [queryClient] = useState(()=> {
+function Providers({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(() => {
     return new QueryClient({
-      defaultOptions:{
-        queries:{
+      defaultOptions: {
+        queries: {
           staleTime: 60 * 1000 * 5,
         },
       },
     });
   });
 
-
   return (
     <>
-    <ThemeProvider 
-    attribute='class'
-    defaultTheme='system'
-    enableSystem
-    disableTransitionOnChange>
-
-    <Toaster/>
-    <QueryClientProvider client={queryClient}>
-    <ReactQueryDevtools initialIsOpen={false}/>
-    {children}
-    </QueryClientProvider>
-    </ThemeProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <Toaster />
+        <QueryClientProvider client={queryClient}>
+          <ReactQueryDevtools initialIsOpen={false} />
+          {children}
+        </QueryClientProvider>
+      </ThemeProvider>
     </>
-)
+  );
 }
-export default providers;
+export default Providers;
