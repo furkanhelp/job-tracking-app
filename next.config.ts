b7175937f02cbc4dccde5664ffd88bc80d576d86
Furkan <frknyrdm0@gmail.com>
2025-06-21 14:38:03 +0300

    v0.9.5

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {},
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
